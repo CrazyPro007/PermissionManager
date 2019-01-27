8f373bc8816e70c313bc00edea3b205eaf3f5dcd
Shivank Agarwal <shivankagarwal@Shivank-Agarwal594-MPR00190.local>
2019-01-27 19:47:49 +0530

    PermissionManager added [ Notification Type permission only ]

---
+++ PermissionManager/PermissionManager/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PermissionManager
+//
+//  Created by Shivank Agarwal on 27/01/19.
+//  Copyright © 2019 Shivank Agarwal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        setPermissionManager()
+    }
+}
+
---
+++ PermissionManager/README.md
@@ -0,0 +1,6 @@
+
+# PermissionManager
+
+## About Project
+
+A repo that handle the permission asked by iOS from user like notification. 
